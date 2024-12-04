0f4649043110e194bcaf08d5ae82be3d8b027935
Renato Ceolin <renato.ceolin@renatoceolin.com>
2024-12-04 11:58:42 -0300

    remove bun support

--- bun.zsh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env zsh
-
-# bun completions
-[ -s "/home/renatoceolin/.bun/_bun" ] && source "/home/renatoceolin/.bun/_bun"
-
-# bun
-export BUN_INSTALL="$HOME/.bun"
-export PATH="$BUN_INSTALL/bin:$PATH"
