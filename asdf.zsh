898edcbc1fc14824ff6580a220ef2209bfdfb24a
Renato Ceolin <renato.ceolin@renatoceolin.com>
2025-02-12 11:21:43 -0300

    hybrid asdf setup

--- asdf.zsh
+++ asdf.zsh
@@ -1,3 +1,10 @@
 if [ -f "$HOME/.asdf/asdf.sh" ]; then
-  . "$HOME/.asdf/asdf.sh"
+    . "$HOME/.asdf/asdf.sh"
+else
+    export PATH="${ASDF_DATA_DIR:-$HOME/.asdf}/shims:$PATH"
+
+    # append completions to fpath
+    fpath=(${ASDF_DATA_DIR:-$HOME/.asdf}/completions $fpath)
+    # initialise completions with ZSH's compinit
+    autoload -Uz compinit && compinit
 fi
